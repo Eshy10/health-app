7dc678ba6572ca86b5e07ffca0528e31904fcc05
Eshy10 <adewalebose15@gmail.com>
2020-10-12 19:25:11 +0100

    fix naming error

--- src/containers/homepage/Homepage.js
+++ src/containers/homepage/Homepage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Navbar from '../../components/navbar/Navbar';
 import BottomNavbar from '../../components/bottomNav/bottomNav';
-import MeasureCategoryList from '../../components/measureList/MeasureList';
+import MeasureCategoryList from '../../components/measureList/measureList';
 import Container from '@material-ui/core/Container';
 
 const Homepage = () => {
