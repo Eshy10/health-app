8484584759782f14aa4b076997e0f0be2b22b0ef
Eshy10 <adewalebose15@gmail.com>
2020-10-13 16:25:54 +0100

    modify the colors in the track measurements components

--- src/components/measureDetails/MeasureDetails.styles.js
+++ src/components/measureDetails/MeasureDetails.styles.js
@@ -10,6 +10,7 @@ const useStyles = makeStyles({
     justifyContent: 'space-around',
     alignItems: 'center',
     textAlign: 'center',
+    backgroundColor: 'white',
     boxShadow: '0px 2px 1px -1px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,0.12)',
   },
   title: {
--- src/components/track/trackCard.styles.js
+++ src/components/track/trackCard.styles.js
@@ -15,6 +15,7 @@ const useStyles = makeStyles(theme => ({
     marginRight: '2em',
     marginLeft: '2em',
     padding: '1em',
+    backgroundColor: 'white',
     boxShadow: '0px 2px 1px -1px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,0.12)',
   },
   title: {
